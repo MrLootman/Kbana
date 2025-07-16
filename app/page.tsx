57481cd21a863ce81354d986a781beb8fe07bc3c
NICOLAS MICHEL <nicolas_m_44@hotmail.fr>
2025-07-16 19:12:51 +0200

    First version done

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,176 @@
+import "./page.css";
 import Image from "next/image";
-import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <>
+      <header>
+        <Image src="/logo.png" alt="logo" width="58" height="58" />
+        <nav>
+          <a href="#menus">Menus</a>
+          <a href="#bokits">Bokits</a>
+          <a href="#contact">Contact</a>
+        </nav>
+      </header>
+      <main>
+        <hgroup>
+          <h1>La Kbana Bokit</h1>
+          <h2>Spécialités Caraïbéennes</h2>
+          <Image src="/hibiscus.png" alt="Hibiscus flower" width="100" height="80" />
+        </hgroup>
+        <Image src="/bokits/meal_1.jpg" alt="guadelupean sandwich" width="900" height="600" />
+        <section id="menus">
+          <h2>Menu</h2>
+          <article className="surface outline">
+            <p>
+              <strong>Pain Bokit ou Agoulou</strong> + <strong>Accompagnement</strong> + <strong>Boisson</strong>
+            </p>
+            <p>Prix du plat + 3 €</p>
+          </article>
+        </section>
+        <section id="sandwichs">
+          <h2>Nos plats</h2>
+          <ul className="card-list menu-list">
+            <li>
+              <img src="/bokits/bokits_cards/meal_10_rm.png" width="200" height="138" />
+              <h3>Cuba</h3>
+              <p className="price">8€</p>
+              <p className="recipe">Jambon de poulet, Emmental, Salade, Tomates</p>
+            </li>
+            <li>
+              <img src="/bokits/bokits_cards/meal_11_rm.png" width="200" height="138" />
+              <h3>Gwada</h3>
+              <p className="price">8€</p>
+              <p className="recipe">Jambon, Emmental, Tomates, Salade</p>
+            </li>
+            <li>
+              <img src="/bokits/bokits_cards/meal_5_rm.png" width="200" height="138" />
+              <h3>Dominica</h3>
+              <p className="price">8€</p>
+              <p className="recipe">Saucisse de poulet, Emmental, Salade, Tomates</p>
+            </li>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
+            <li>
+              <img src="/bokits/bokits_cards/meal_6_rm.png" width="200" height="138" />
+              <h3>Montserrat</h3>
+              <p className="price">8€</p>
+              <p className="recipe">Oeuf, Emmental, Tomates, Salade</p>
+            </li>
+            <li>
+              <img src="/bokits/bokits_cards/meal_7_rm.png" width="200" height="138" />
+              <h3>Guyana</h3>
+              <p className="price">8€</p>
+              <p className="recipe">Haricots rouge, Maïs, Avocat, Salade, Tomates</p>
+            </li>
+            <li>
+              <img src="/bokits/bokits_cards/meal_8_rm.png" width="200" height="138" />
+              <h3>Mada</h3>
+              <p className="price">8€</p>
+              <p className="recipe">Poulet mariné (citron, aïl, persil, oignon), Emmental, Tomates, Salade</p>
+            </li>
+            <li>
+              <img src="/bokits/bokits_cards/meal_9_rm.png" width="200" height="138" />
+              <h3>Jamaïca</h3>
+              <p className="price">8€</p>
+              <p className="recipe">Poulet jerk (curcuma, gingembre, aïl, persil), Tomates, Salade</p>
+            </li>
+            <li>
+              <img src="/bokits/bokits_cards/meal_4_rm.png" width="200" height="138" />
+              <h3>Sainte Lucia</h3>
+              <p className="price">8€</p>
+              <p className="recipe">Boeuf haché, Tomates, Salade</p>
+            </li>
+            <li>
+              <img src="/bokits/bokits_cards/meal_11_rm.png" width="200" height="138" />
+              <h3>Haïti</h3>
+              <p className="price">8€</p>
+              <p className="recipe">Boeuf haché mariné (curcuma, gingembre, aïl, persil), Salade, Tomates</p>
+            </li>
+            <li>
+              <img src="/bokits/bokits_cards/meal_9_rm.png" width="200" height="138" />
+              <h3>Antigua</h3>
+              <p className="price">8€</p>
+              <p className="recipe">Morue, Salade, Tomates</p>
+            </li>
+            <li>
+              <img src="/bokits/bokits_cards/meal_4_rm.png" width="200" height="138" />
+              <h3>Antillaise</h3>
+              <p className="price">8€</p>
+              <p className="recipe">Boeuf haché, Poulet, Saucisse de poulet, Oeuf, Emmental, Salade, Tomates</p>
+            </li>
+            <li>
+              <img src="/bokits/bokits_cards/meal_5_rm.png" width="200" height="138" />
+              <h3>West Indies</h3>
+              <p className="price">8€</p>
+              <p className="recipe">Boeuf haché, Jambon, Saucisse de poulet, Oeuf, Emmental, Salade, Tomates</p>
+            </li>
+          </ul>
+        </section>
+        <section className="surface carousel-container">
+          <Image src="/assets/leave.png" alt="leave" height="180" width="120" />
+          <h2>Nos boissons</h2>
+          <div className="carousel">
+            <ul className="group">
+              <li><Image src="/drinks/drinks_cards/drink_1.png" alt="Bokit" width="148" height="120" /></li>
+              <li><Image src="/drinks/drinks_cards/drink_2.png" alt="Bokit" width="148" height="120" /></li>
+              <li><Image src="/drinks/drinks_cards/drink_4.png" alt="Bokit" width="148" height="120" /></li>
+              <li><Image src="/drinks/drinks_cards/drink_7.png" alt="Bokit" width="148" height="120" /></li>
+              <li><Image src="/drinks/drinks_cards/drink_12.png" alt="Bokit" width="148" height="120" /></li>
+            </ul>
+            <ul aria-hidden className="group">
+              <li><Image src="/drinks/drinks_cards/drink_1.png" alt="Bokit" width="148" height="120" /></li>
+              <li><Image src="/drinks/drinks_cards/drink_2.png" alt="Bokit" width="148" height="120" /></li>
+              <li><Image src="/drinks/drinks_cards/drink_4.png" alt="Bokit" width="148" height="120" /></li>
+              <li><Image src="/drinks/drinks_cards/drink_7.png" alt="Bokit" width="148" height="120" /></li>
+              <li><Image src="/drinks/drinks_cards/drink_12.png" alt="Bokit" width="148" height="120" /></li>
+            </ul>
+          </div>
+        </section>
+        <section className="surface satisfaction">
+          <hgroup>
+            <h2>Votre satisfaction notre priorité</h2>
+            <p>Votre satisfaction est notre priorité absolue. Nous mettons tout en œuvre pour vous offrir une expérience culinaire unique et mémorable. N'hésitez pas à nous faire part de vos impressions et suggestions, car votre avis compte énormément pour nous.</p>
+            <a className="surface outline" href="https://www.google.com/search?sca_esv=6a136238722af36a&sxsrf=AE3TifPmFALXgbGSAIcN9yONqLHXbt_RJg:1752668512107&si=AMgyJEtREmoPL4P1I5IDCfuA8gybfVI2d5Uj7QMwYCZHKDZ-E0o_2yfIvzgS4dM4J0Iyzk3NnKjcNfMQOajADYVtpwzZ6i8BuAzIoHSqaA9IIZpnmYHQ_gb-JRugDjozJEqTHcXZ87HP&q=KBANA+BOKIT+Avis&sa=X&ved=2ahUKEwjo28qAr8GOAxVHVaQEHV8dEGAQ0bkNegQISBAE&biw=1229&bih=584&dpr=1.56" target="_blank">Ajoutez un avis Google</a>
+          </hgroup>
+        </section>
       </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+
+
+      <footer>
+        <h2 className="surface">Contact</h2>
+        <section>
+          <h3>Nous contacter</h3>
+          <address>
+            <span>Numéro : <a href="tel:0255095929">02 55 09 59 29</a></span>
+          </address>
+        </section>
+        <section>
+          <h3>Ouvertures</h3>
+          <ul>
+            <li>
+              <span>Lundi : /</span>
+            </li>
+            <li>
+              <span>Mardi : /</span>
+            </li>
+            <li>
+              <span>Mercredi : /</span>
+            </li>
+            <li>
+              <span>Jeudi : 19H - 22H</span>
+            </li>
+            <li>
+              <span>Vendredi : 19H - 22H</span>
+            </li>
+            <li>
+              <span>Samedi : 19H - 22H</span>
+            </li>
+            <li>
+              <span>Dimanche : /</span>
+            </li>
+          </ul>
+        </section>
       </footer>
-    </div>
+    </>
   );
 }
