59a241a26aa4ba8adcaaf1fe6679990660009fea
NICOLAS MICHEL <nicolas_m_44@hotmail.fr>
2025-08-05 13:31:08 +0200

    &apos; error fixed

--- app/page.tsx
+++ app/page.tsx
@@ -199,8 +199,7 @@ export default function Home() {
                   <Image className="" src="/assets/reviews-stars.webp" alt="" width="80" height="15" />
                 </div>
 
-                <p>J'ai pu découvrir ce restaurant, je suis venu avec mon chiot ça n'a posé de problème à personne j'ai même été très bien accueilli avec une gamelle d'eau pour le loulou.</p>
-
+                <p>J&apos;ai pu découvrir ce restaurant, je suis venu avec mon chiot ça n&apos;a posé de problème à personne j&apos;ai même été très bien accueilli avec une gamelle d'eau pour le loulou.</p>
               </li>
 
 
