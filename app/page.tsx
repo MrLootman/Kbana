b90f53ec5dd2b63ab50847e30a586856a0a8d877
NICOLAS MICHEL <nicolas_m_44@hotmail.fr>
2025-09-15 11:04:11 +0200

    🚀 Cards improved and footer upgraded

--- app/page.tsx
+++ app/page.tsx
@@ -104,24 +104,28 @@ export default function Home() {
         </div>
         <section id="menus">
           <Image src="/assets/hibiscus.webp" alt="Hibiscus flower" width="480" height="480" />
-          <h2>Menus</h2>
+          <h2><strong>Découvrez</strong> nos saveurs</h2>
           <article className="surface outline">
             <h3>Classique</h3>
             <p>
-              <strong>Pain Bokit ou Agoulou</strong> + <strong>Accompagnement</strong> + <strong>Boisson</strong>
+              <strong>Pain Bokit ou Agoulou</strong>
             </p>
-            <p>Prix du plat + 3 €</p>
+            <p>+ Accompagnement</p>
+            <p>+ Boisson</p>
+            <p>≃ 12 €</p>
           </article>
           <article className="surface outline">
             <h3>Ti Moun&apos;</h3>
             <p>
-              <strong>Petit Bokit (Gwada, Cuba, Dominica, Montserrat)</strong> + <strong>Petite frite</strong> + <strong>Caprisun ou Cristalline</strong>
+              <strong>Petit Bokit (Gwada, Cuba, Dominica, Montserrat)</strong>
             </p>
+            <p>+ Petite frite</p>
+            <p>+ Caprisun ou Cristalline</p>
             <p>7 €</p>
           </article>
         </section>
         <section id="sandwichs">
-          <h2>Nos plats</h2>
+          <h2><strong>Découvrez</strong> nos plats</h2>
           <ul className="card-list menu-list">
             {meals.map((el) => {
               return (
@@ -147,64 +151,62 @@ export default function Home() {
           <a href="https://google.com/search?sca_esv=e948bafec9f39f8c&amp;sxsrf=AE3TifPz8O6EGrAfp61P6Mk-lQwKyfLdew:1753794756728&amp;uds=AOm0WdE2fekQnsyfYEw8JPYozOKzqpex0tSvmKdffwhThOpjgxvZnCM0wVyGTSXAVA7JF1spOSJqpFyAOFHCS0Y_YoQmW_CTt6WQWUng5X3xThXXgQvlqevkU-6nMRNyb-t3A3QEvGeD8FPtqIXf3I2vvw4k_cxctA&amp;si=AMgyJEtREmoPL4P1I5IDCfuA8gybfVI2d5Uj7QMwYCZHKDZ-E0o_2yfIvzgS4dM4J0Iyzk2TGBs_hLU9kmJiHGuYdnVBfmTyVWRf3QD2YQxH2wz1TgWVets%3D&amp;q=KBANA+BOKIT+Avis&amp;sa=X&amp;ved=2ahUKEwj2k-_LkuKOAxUoT6QEHYYYJUEQ3PALegQITRAF" target="no-referrer">204&nbsp;avis</a>
         </div>
         <section className="surface satisfaction">
-          <hgroup>
-            <h2>Votre satisfaction notre priorité</h2>
+          <h2>Votre satisfaction notre priorité</h2>
 
-            <ul className="review-list">
-              <li>
-                <p>P</p>
+          <ul className="review-list">
+            <li>
+              <p>P</p>
 
-                <div className="author">
-                  <p>Jean-Philippe</p>
-                  <p>14 Août 2025</p>
-                </div>
+              <div className="author">
+                <p>Jean-Philippe</p>
+                <p>14 Août 2025</p>
+              </div>
 
-                <Image src="/assets/google-logo.webp" alt="Google logo" width="16" height="16" />
+              <Image src="/assets/google-logo.webp" alt="Google logo" width="16" height="16" />
 
-                <div className="stars">
-                  <Image className="" src="/assets/reviews-stars.webp" alt="" width="80" height="15" />
-                </div>
+              <div className="stars">
+                <Image className="" src="/assets/reviews-stars.webp" alt="" width="80" height="15" />
+              </div>
 
-                <p>J&apos;ai pu découvrir ce restaurant, je suis venu avec mon chiot ça n&apos;a posé de problème à personne j&apos;ai même été très bien accueilli avec une gamelle d&apos;eau pour le loulou.</p>
-              </li>
-              <li>
-                <p>P</p>
-                <div className="author">
-                  <p>Pages Jaunes</p>
-                  <p>29 Juillet 2021</p>
-                </div>
+              <p>J&apos;ai pu découvrir ce restaurant, je suis venu avec mon chiot ça n&apos;a posé de problème à personne j&apos;ai même été très bien accueilli avec une gamelle d&apos;eau pour le loulou.</p>
+            </li>
+            <li>
+              <p>P</p>
+              <div className="author">
+                <p>Pages Jaunes</p>
+                <p>29 Juillet 2021</p>
+              </div>
 
-                <Image src="/assets/google-logo.webp" alt="Google logo" width="16" height="16" />
+              <Image src="/assets/google-logo.webp" alt="Google logo" width="16" height="16" />
 
-                <div className="stars">
-                  <Image className="" src="/assets/reviews-stars.webp" alt="" width="80" height="15" />
-                </div>
+              <div className="stars">
+                <Image className="" src="/assets/reviews-stars.webp" alt="" width="80" height="15" />
+              </div>
 
-                <p>un vrai délice, je n&apos;en ai jamais mangé d&apos;aussi bon aux Antilles ni ailleurs...</p>
-              </li>
-              <li>
-                <p>P</p>
+              <p>un vrai délice, je n&apos;en ai jamais mangé d&apos;aussi bon aux Antilles ni ailleurs...</p>
+            </li>
+            <li>
+              <p>P</p>
 
-                <div className="author">
-                  <p>Phanie973</p>
-                  <p>24 Juin 2021</p>
-                </div>
+              <div className="author">
+                <p>Phanie973</p>
+                <p>24 Juin 2021</p>
+              </div>
 
-                <Image src="/assets/google-logo.webp" alt="Google logo" width="16" height="16" />
+              <Image src="/assets/google-logo.webp" alt="Google logo" width="16" height="16" />
 
-                <div className="stars">
-                  <Image className="" src="/assets/reviews-stars.webp" alt="" width="80" height="15" />
-                </div>
+              <div className="stars">
+                <Image className="" src="/assets/reviews-stars.webp" alt="" width="80" height="15" />
+              </div>
 
-                <p>Propriétaire super sympathique et souriant. On a passé un superbe moment pour l&apos;anniversaire de mon fils et surtout on a trop bien mangé 😋 </p>
-              </li>
-
+              <p>Propriétaire super sympathique et souriant. On a passé un superbe moment pour l&apos;anniversaire de mon fils et surtout on a trop bien mangé 😋 </p>
+            </li>
 
 
-            </ul>
 
-            <a className="surface outline" href="https://www.google.com/search?sca_esv=6a136238722af36a&sxsrf=AE3TifPmFALXgbGSAIcN9yONqLHXbt_RJg:1752668512107&si=AMgyJEtREmoPL4P1I5IDCfuA8gybfVI2d5Uj7QMwYCZHKDZ-E0o_2yfIvzgS4dM4J0Iyzk3NnKjcNfMQOajADYVtpwzZ6i8BuAzIoHSqaA9IIZpnmYHQ_gb-JRugDjozJEqTHcXZ87HP&q=KBANA+BOKIT+Avis&sa=X&ved=2ahUKEwjo28qAr8GOAxVHVaQEHV8dEGAQ0bkNegQISBAE&biw=1229&bih=584&dpr=1.56" target="_blank">Ajoutez un avis Google</a>
-          </hgroup>
+          </ul>
+
+          <a className="surface outline" href="https://www.google.com/search?sca_esv=6a136238722af36a&sxsrf=AE3TifPmFALXgbGSAIcN9yONqLHXbt_RJg:1752668512107&si=AMgyJEtREmoPL4P1I5IDCfuA8gybfVI2d5Uj7QMwYCZHKDZ-E0o_2yfIvzgS4dM4J0Iyzk3NnKjcNfMQOajADYVtpwzZ6i8BuAzIoHSqaA9IIZpnmYHQ_gb-JRugDjozJEqTHcXZ87HP&q=KBANA+BOKIT+Avis&sa=X&ved=2ahUKEwjo28qAr8GOAxVHVaQEHV8dEGAQ0bkNegQISBAE&biw=1229&bih=584&dpr=1.56" target="_blank">Ajoutez un avis Google</a>
         </section>
       </main>
 
@@ -212,38 +214,55 @@ export default function Home() {
       <footer>
         <h2 className="surface">Contact</h2>
         <section>
-          <h3>Nous contacter</h3>
-          <address>
-            <span>Numéro : <a href="tel:0255095929">02 55 09 59 29</a></span>
-          </address>
+          <h3>La Kbana Bokit</h3>
+          <h2>Informations Pratiques</h2>
         </section>
-        <section>
-          <h3>Ouvertures</h3>
-          <ul>
-            <li>
-              <span>Lundi : /</span>
-            </li>
-            <li>
-              <span>Mardi : /</span>
-            </li>
-            <li>
-              <span>Mercredi : /</span>
-            </li>
-            <li>
-              <span>Jeudi : 19H - 22H</span>
-            </li>
-            <li>
-              <span>Vendredi : 19H - 22H</span>
-            </li>
-            <li>
-              <span>Samedi : 19H - 22H</span>
-            </li>
-            <li>
-              <span>Dimanche : /</span>
-            </li>
-          </ul>
-        </section>
-      </footer>
+
+        <table>
+          <thead>
+            <tr>
+              <th>Horaires</th>
+              <th>Coordonnées</th>
+              <th>Options de paiement</th>
+              <th>Services</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <p>Jeudi au Samedi</p>
+                <p>19:00 - 22:00</p>
+              </td>
+
+              <td>
+                <p>Restaurant Kbana Bokit</p>
+                <p>57 rue Jean Jaurès, 44400 Rezé - <a href="tel:0255095929">02 55 09 59 29</a></p>
+              </td>
+
+              <td>
+                <ul>
+                  <li>Paiement sans contact</li>
+                  <li>Règlement en espèces</li>
+                  <li>Carte bancaire, Visa, Mastercard</li>
+                  <li>Cartes Maestro & AMEX</li>
+                  <li>Ticket restaurant</li>
+                  <li>Carte ticket restaurant</li>
+                  <li>Apple Pay</li>
+                </ul>
+              </td>
+
+              <td>
+                <ul>
+                  <li>Accessible aux personnes à mobilité réduite</li>
+                  <li>Animaux domestiques autorisés</li>
+                  <li>Tables en terrasse</li>
+                  <li>Wi-Fi gratuit</li>
+                </ul>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </footer >
     </>
   );
 }
